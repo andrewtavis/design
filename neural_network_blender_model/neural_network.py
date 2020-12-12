bd9686c33796c968aaa07651775c18b99cf87e0d
Andrew McAllister <andrew.t.mcallister@gmail.com>
2020-12-12 14:36:53 +0100

    Add .py of neural network model

--- neural_network_blender_model/neural_network.py
+++ neural_network_blender_model/neural_network.py
@@ -1,21 +1,7 @@
-<div align="center">
-  <a href="https://github.com/andrewtavis/design/tree/main/neural_network_blender_model"><img src="https://github.com/andrewtavis/design/blob/main/resources/gh_images/neural_net_logo.png" width="638" height="200"></a>
-</div>
+# codes
+# https://blenderscripting.blogspot.com
+# https://docs.blender.org/api/current/
 
---------------------------------------
-
-### Making a 3D model of a nueral network using Python and Blender
-
-**Jump to:** [Process](#process) • [Result](#result)
-
-# Explanation
-This directory takes you through the steps to make a 3D model of a [neural network](https://en.wikipedia.org/wiki/Neural_network) using Python and [Blender](https://www.blender.org/). The final result includes a base that it can stand on after being 3D printed.
-
-# Process
-
-### Imports and basics
-
-```python
 import bpy
 import bmesh
 import math
@@ -35,11 +21,8 @@ node_coordinate_dict = {}
 
 def cube(number): 
     return number * number * number 
-```
 
-### Make the Nodes
 
-```python
 def make_nn_nodes(network_layers, nodes_per_layer):
     """
     Creates neural network nodes and saves their coordinates for later connection
@@ -95,11 +78,8 @@ def make_nn_nodes(network_layers, nodes_per_layer):
                            
                 node_identifier = '{}|{}'.format(str(iteration), str(i))
                 node_coordinate_dict[node_identifier] = [coordinate_x, 0, coordinate_z]
-```
 
-### Connect Node Layers
 
-```python
 def cylinder_between(x1, y1, z1, x2, y2, z2, r):
   """
   Makes a cylander connector between neural network nodes
@@ -140,10 +120,8 @@ def connect_nn_nodes(network_layers, nodes_per_layer):
                                  node_coordinate_dict['{}|{}'.format(str(i+1), str(index_right))][1], 
                                  node_coordinate_dict['{}|{}'.format(str(i+1), str(index_right))][2], 
                                  node_radius/4)
-```
 
-### Make a Base (optional - for 3D printing)
-```python
+
 def find_z_dims(network_layers): 
     """
     Finds all layer lowest z-dimensions for adding the base
@@ -187,16 +165,10 @@ def create_base(network_layers, z_dims):
                                                         central_layers_bottom_node_left[1], 
                                                         min(z_dims) - node_radius)
                                             )
-```
 
-### Putting it All Together
-
-The model is made by running all the previous functions with defined inputs. The inputs here make a very basic network that resembles the [Millennium Falcon](https://en.wikipedia.org/wiki/Millennium_Falcon) (not intentional, but awesome).
-
-```python
 # Define the number of nodes of the network and the node count per layer
-num_layers = 5
-num_layer_nodes = [3,6,6,4,2]
+num_layers = 6
+num_layer_nodes = [4,10,10,10,10, 2]
 
 # Run the functions to create the model
 make_nn_nodes(num_layers, num_layer_nodes)        
@@ -206,10 +178,3 @@ connect_nn_nodes(num_layers, num_layer_nodes)
 layer_lower_z_dims = find_z_dims(num_layers)
 
 create_base(num_layers, layer_lower_z_dims)
-```
-
-# Result
-
-A zip of the final .blend file can be found [here](https://github.com/andrewtavis/design/blob/main/neural_network_blender_model/neural_network.blend.zip), a .py file for the codes is [here](https://github.com/andrewtavis/design/blob/main/neural_network_blender_model/neural_network.py), and the .stl file below is [here](https://github.com/andrewtavis/design/blob/main/neural_network_blender_model/neural_network.stl).
-
-![](https://raw.githubusercontent.com/andrewtavis/design/main/resources/gh_images/neural_network_stl.gif)
